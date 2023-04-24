05e2b75d6124ea5fe529078ee254e906ddd519e1
phillipnzau <nzauonline78@gmail.com>
2023-04-24 17:30:20 +0300

    set up

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primaryRed: 'hsl(351 80% 49%)',
+        primaryBlack: 'hsl(0 0% 5%)',
+        primaryGray: 'hsl(224 15% 14%)',
+        primaryWhite: 'hsl(240 5% 92%)',
+      },
+      fontFamily: {
+        'textFont': ['zonaRegular', 'sans-serif'],
+        'textLightFont': ['zonaLight', 'sans-serif'],
+        'headingFont': ['zonaBold', 'serif'],
+        'headingExtraBold': ['zonaExtraBold', 'serif'],
+      },
+    },
   },
   plugins: [],
 }
