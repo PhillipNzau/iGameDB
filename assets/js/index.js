352b13fb0be3efce01b5cb051d6646040820d994
phillipnzau <nzauonline78@gmail.com>
2023-04-25 10:24:30 +0300

    search animation

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,30 +1,16 @@
-'use strict'
-
 
 // Usage
 /*GET https://api.rawg.io/api/platforms?key=YOUR_API_KEY*/
 
 const rawgAPI = '462e2afdaf9f4f94ac3f237773ee1818';
+const searchButton = document.getElementById('searchBtn');
+const searchGameInput = document.getElementById('searchGame');
+const html = document.querySelector('html');
 
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
+let isOpen = false
+
+const handleClick = ()=> {
   
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  });
-  
+}
+
+searchButton.addEventListener('click',handleClick);
